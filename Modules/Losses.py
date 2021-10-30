858700c13375140f3b5eb7bb8cbde2864c68b770
jiseong.kim <intelli8786@gmail.com>
2021-10-31 01:59:07 +0900

    loss 추가

--- Modules/Losses.py
+++ Modules/Losses.py
@@ -1,7 +1,94 @@
 '''
 이 파일에 학습에 사용될 Loss를 정의합니다.
 '''
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-def CrossEntropyLoss():
-    return nn.CrossEntropyLoss()
+
+from torch.nn import CrossEntropyLoss
+
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        dice = (2. * intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+
+        return 1 - dice
+
+
+class DiceCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        # flatten label and prediction tensors
+        num_classes = inputs.size(1)
+        true_1_hot = torch.eye(num_classes)[targets]
+
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(inputs, dim=1)
+
+        true_1_hot = true_1_hot.type(inputs.type())
+        dims = (0,) + tuple(range(2, targets.ndimension()))
+        intersection = torch.sum(probas * true_1_hot, dims)
+        cardinality = torch.sum(probas + true_1_hot, dims)
+        dice_loss = (2. * intersection / (cardinality + 1e-7)).mean()
+        dice_loss = (1 - dice_loss)
+
+        ce = F.cross_entropy(inputs, targets, reduction='mean')
+        dice_bce = ce * 0.75 + dice_loss * 0.25
+        return dice_bce
+
+
+class IOULOss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IOULOss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection
+
+        IOU = (intersection + smooth) / (union + smooth)
+        return 1 - IOU
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.5, gamma=2, weight=None, size_average=True):
+        super(FocalLoss, self).__init__()
+
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def forward(self, inputs, targets, smooth=1):
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        BEC_EXP = torch.exp(-BCE)
+        focal_loss = self.alpha * (1 - BEC_EXP) ** self.gamma * BCE
+        return focal_loss
+
+
+class TverskyLoss(nn.Module):
+    def __init__(self, alpha=0.5, beta=0.5, weight=None, size_average=True):
+        super(TverskyLoss, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, inputs, targets, smooth=1):
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+
+        TP = (inputs * targets).sum()
+        FP = ((1 - targets) * inputs).sum()
+        FN = (targets * (1 - inputs)).sum()
+
+        Tversky = (TP + smooth) / (TP + self.alpha * FP + self.beta * FN + smooth)
+        return 1 - Tversky
