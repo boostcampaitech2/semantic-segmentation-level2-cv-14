d89b5eafdc2e87bf46550010f8751bff75738931
jiseong.kim <intelli8786@gmail.com>
2021-10-31 01:58:50 +0900

    Transform 추가

--- Modules/Transforms_Preprocess.py
+++ Modules/Transforms_Preprocess.py
@@ -26,6 +26,15 @@ def HorizontalFlip_Rotate90():
         ToTensorV2()
     ])
 
+def HorizontalFlip_Rotate90_Elastic():
+    return alb.Compose([
+        alb.HorizontalFlip(p=0.5),
+        alb.RandomRotate90(p=0.5),
+        alb.ElasticTransform(p=0.5, alpha=40, sigma=40 * 0.05, alpha_affine=40 * 0.03),
+        alb.Normalize(),
+        ToTensorV2()
+    ])
+
 def HorizontalFlip_Rotate90_CLAHE():
     return alb.Compose([
         alb.HorizontalFlip(p=0.5),
